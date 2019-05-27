6c07f15ee9efe78976acedaa9f61ba8890be8ea4
wangwei003 <wangwei003@zhongan.com>
2019-05-27 14:27:43 +0800

    [00] add TestClient.

--- src/main/java/cn/parlife/training/nio/issue/selectinfinitely/TestClient.java
+++ src/main/java/cn/parlife/training/nio/issue/selectinfinitely/TestClient.java
@@ -1,7 +1,5 @@
 package cn.parlife.training.nio.issue.selectinfinitely;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -29,8 +27,8 @@ public class TestClient {
 	 
 	 public void start() throws Throwable {
 		 Socket socket = new Socket(host, port);
-		 BufferedReader in = new BufferedReader(
-				    new InputStreamReader(socket.getInputStream()));
+		 /*BufferedReader in = new BufferedReader(
+				    new InputStreamReader(socket.getInputStream()));*/
 		 PrintWriter out = new PrintWriter(
 				    new OutputStreamWriter(socket.getOutputStream()),
 				    true /* auto flush */);
